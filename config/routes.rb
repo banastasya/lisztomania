93133523fc5e5072310936077697d3036cb829e6
Andrew Lever <andylever28@gmail.com>
2016-11-10 18:22:04 -0500

    updated home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'main_page' => 'static#main_page'
+  root 'static#main_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
